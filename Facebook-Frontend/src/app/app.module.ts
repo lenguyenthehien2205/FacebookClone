1a81c82b46adcbaaceecc95a6d681e6e3e13f4e0
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-08-23 23:32:23 +0700

    push

--- Facebook-Frontend/src/app/app.module.ts
+++ Facebook-Frontend/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { CustomTitleComponent } from "./components/shared/custom-title/custom-title.component";
 
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    CustomTitleComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ Facebook-Frontend/src/app/components/shared/custom-title/custom-title.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-title.component.html',
+  styleUrl: './custom-title.component.css'
+})
+export class CustomTitleComponent {
+  titleCustom = input.required();
+}
